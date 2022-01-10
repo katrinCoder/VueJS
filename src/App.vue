f5905e16c0be0eae45706d7a5f24bf8c848c7ddb
Katrin <katrin.krotova@mail.ru>
2022-01-10 23:05:24 +0300

    HomeWork3

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CalculatorComp/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import CalculatorComp from '@/components/Calculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    CalculatorComp
+  },
+  data: () => ({
+    counter: 0
+  }),
+  methods: {
+    // increase: function (step, event) {
+    //   // eslint-disable-next-line no-plusplus
+    //   this.counter += step;
+    //   console.log(event);
+    // },
+    increase (step, event) {
+      // eslint-disable-next-line no-plusplus
+      this.counter += step
+      console.log(event)
+    },
+    resetCounter () {
+      this.counter = 0
+    },
+    blur () {
+      console.log('blur')
+    },
+    focus () {
+      console.log('focus')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Keyboard.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="keyboard">
+        <button class="key" v-for="number in numbers" :key="number" @click="write(number)">{{ number }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Keyboard',
+  data: () => ({
+    toggle: '',
+    numbers: ['', '', 'C', '7', '8', '9', '4', '5', '6', '1', '2', '3', '', '0', ''],
+    keyValue: ''
+  }),
+  methods: {
+    write (number) {
+      if (number !== '') {
+        this.keyValue = number
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.keyboard {
+  display: grid;
+  width: 150px;
+  height: 250px;
+  grid-template-rows: repeat(5, 50px);
+  grid-template-columns: repeat(3, 50px);
+  margin-top: 5px;
+  margin-left: 4px;
+}
+.longKey {
+  grid-column: span 2;
+}
+</style>
